b45fa3e3464742e3fea551ced27250ae6420e676
Yubaraj Dey <143158304+yubaraj001@users.noreply.github.com>
2024-08-07 21:07:29 +0530

    Add files via upload

---
+++ pattern1.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class ex1{
+    public static void main(String args[]){
+     Scanner sc=new Scanner(System.in);
+     System.out.println("Enter a range");
+     int a=sc.nextInt();
+     for(int i=1;i<=a;i++){
+        for(int j=1;j<=i;j++){
+            System.out.print("*");
+        }
+        System.out.print("\n");
+     }
+    }
+}
